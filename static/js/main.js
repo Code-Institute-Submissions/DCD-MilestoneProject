2a0b70e6f473e5add9608ebc2cf1bccdc11c9eb1
Colman Leung <comacoma0218@hotmail.com>
2018-07-21 20:58:17 +0000

    Change format of cuisine (chips to individual fields)

--- static/js/main.js
+++ static/js/main.js
@@ -7,6 +7,28 @@ $(document).ready(function() {
     });
     $('.tooltipped').tooltip();
 
+    $('.add_cuisine').click(function() {
+
+        var last = $(".cuisine:last").attr("id");
+        var split_id = last.split("_");
+        var next = Number(split_id[1]) + 1;
+
+        $(".cuisine:last").after('<div class="cuisine" id="cuisine_' + next + '"></div>');
+        $('#cuisine_' + next).append(
+            '<input type="text" name="cuisine_' + next + '" placeholder="Cuisine" minlength="2" maxlength="20" class="col s8" required/>' +
+            '<a class="btn waves-effect waves-light red remove_cuisine col s4" id="remove_' + next + '"><i class="material-icons">remove</i></a>'
+        );
+    });
+    
+     $('#cuisines_container').on('click', '.remove_cuisine', function() {
+        var id = this.id;
+        var split_id = id.split("_");
+        var deleteindex = split_id[1];
+
+        $("#cuisine_" + deleteindex).remove();
+
+    });
+
     $('.add_ingredient').click(function() {
 
         var last = $(".ingredient:last").attr("id");
@@ -15,9 +37,9 @@ $(document).ready(function() {
 
         $(".ingredient:last").after('<div class="ingredient" id="ingredient_' + next + '"></div>');
         $("#ingredient_" + next).append(
-            '<input type="text" name="ingredient_' + next + '" placeholder="Ingredient" class="col s7" required pattern="\S+" title="No preceding and ending white spaces" />' +
-            '<input type="text" name="unit_' + next + '" placeholder="Unit" class="col s3" required pattern="\S+" title="No preceding and ending white spaces" />' +
-            '<a class="btn waves-effect waves-light red col s2 remove_ingredient" id="remove_' + next + '"><i class="material-icons">remove</i></a>'
+            '<input type="text" name="ingredient_' + next + '" placeholder="Ingredient" class="col s6" required/>' +
+            '<input type="text" name="unit_' + next + '" placeholder="Unit" class="col s3" required/>' +
+            '<a class="btn waves-effect waves-light red col s3 remove_ingredient" id="remove_' + next + '"><i class="material-icons">remove</i></a>'
         );
     });
 
@@ -29,7 +51,7 @@ $(document).ready(function() {
         $("#ingredient_" + deleteindex).remove();
 
     });
-    
+
     $('.add_instruction').click(function() {
 
         var last = $(".instruction:last").attr("id");
@@ -38,11 +60,11 @@ $(document).ready(function() {
 
         $(".instruction:last").after('<div class="instruction" id="instruction_' + next + '"></div>');
         $("#instruction_" + next).append(
-            '<textarea class="materialize-textarea col s11" placeholder="Instruction" name="instruction_' + next + '"></textarea>' +
-            '<a class="btn waves-effect waves-light red col s1 remove_instruction" id="remove_' + next + '"><i class="material-icons">remove</i></a>'
+            '<textarea class="materialize-textarea col s10" placeholder="Instruction" name="instruction_' + next + '"></textarea>' +
+            '<a class="btn waves-effect waves-light red col s2 remove_instruction" id="remove_' + next + '"><i class="material-icons">remove</i></a>'
         );
     });
-    
+
     $('#instructions_container').on('click', '.remove_instruction', function() {
         var id = this.id;
         var split_id = id.split("_");
