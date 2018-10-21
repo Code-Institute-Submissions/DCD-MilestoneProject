8d4abe064e8bc3976e60e3b3319039e5606364fa
Colman Leung <comacoma0218@hotmail.com>
2018-10-21 16:12:00 +0100

    Update tests

--- test_app.py
+++ test_app.py
@@ -131,6 +131,52 @@ class TestRoute(unittest.TestCase):
         response = self.app.get('/recipes_by_origin/Japan/1', content_type="html/text")
         self.assertIn(b'<h2>Recipes from Japan</h2>', response.data)
 
+    def test_view_recipe_with_invalid_id(self):
+        response = self.app.get('/view_recipe/111122223333444455556666', follow_redirects=True)
+        self.assertIn(b'Invalid recipe id', response.data)
+
+    def test_edit_recipe_with_invalid_id(self):
+        response = self.app.get('/edit_recipe/111122223333444455556666', follow_redirects=True)
+        self.assertIn(b'Invalid recipe id', response.data)
+
+    def test_update_recipe_with_invalid_id(self):
+        testdata = {
+            "name": "test recipe",
+            "origin": "Japan",
+            "cuisine": "Japanese",
+            "ingredient_1": "ingredient 1",
+            "unit_1": "unit 1",
+            "ingredient_2": "ingredient 2",
+            "unit_2": "unit 2",
+            "instruction_1": "instruction 1"
+        }
+        response = self.app.post('/update_recipe/111122223333444455556666', data=testdata, follow_redirects=True)
+        self.assertIn(b'Invalid recipe id', response.data)
+
+    def test_delete_recipe_with_invalid_id(self):
+        response = self.app.get('/delete_recipe/111122223333444455556666', follow_redirects=True)
+        self.assertIn(b'Invalid recipe id', response.data)
+
+    def test_upvote_with_invalid_id(self):
+        with self.app.session_transaction() as sess:
+            sess['username'] = 'admin' # Assumed user 'admin' logged in
+        response = self.app.get('/upvote/111122223333444455556666', follow_redirects=True)
+        self.assertIn(b'Invalid recipe id', response.data)
+
+    def test_user_page_with_invalid_username(self):
+        response = self.app.get('/user/nonuser', follow_redirects=True)
+        self.assertIn(b'No such user', response.data)
+
+    def test_user_page_when_not_logged_in(self):
+        response = self.app.get('/user/admin', follow_redirects=True)
+        self.assertIn(b'<h2>User Login</h2>', response.data) # Redirected to login page
+
+    def test_user_page_invalid_access(self):
+        with self.app.session_transaction() as sess:
+            sess['username'] = 'admin' # Assumed user 'admin' logged in
+        response = self.app.get('/user/colman')
+        self.assertEqual(response.status_code, 302) # Redirected to admin's user page
+
 """
 To test whether user authentication behaves as expected
 """
@@ -139,7 +185,6 @@ class TestUserAuthentication(unittest.TestCase):
         self.app = app.test_client()
         app.config['SERVER_NAME'] = 'localhost.localdomain'
         app.config['SECRET_KEY'] = 'secret_key'
-        return app
 
     def test_login(self):
         response = self.app.post('/login', data=dict(username='admin', password='admin'), follow_redirects=True)
@@ -153,7 +198,6 @@ class TestUserAuthentication(unittest.TestCase):
         response = self.app.post('/register', data=dict(username='testuser', password='testuser'), follow_redirects=True)
         self.assertIn(b'<h3 class="white-text">Hi, testuser</h3>', response.data)
         # Removing test data after assertion
-        mongo = PyMongo(app)
         testdata = mongo.db.users.find_one({'username': 'testuser'})
         if testdata:
             mongo.db.users.delete_one(testdata)
@@ -174,7 +218,6 @@ class TestRecipesCRUD(unittest.TestCase):
         self.app = app.test_client()
         app.config['SERVER_NAME'] = 'localhost.localdomain'
         app.config['SECRET_KEY'] = 'secret_key'
-        return app
 
     def test_add_recipe_as_guest(self):
         testdata = {
@@ -191,9 +234,9 @@ class TestRecipesCRUD(unittest.TestCase):
         self.assertTrue(response.status_code, 302) # Redirected to view_recipe page
         self.assertIn(b'Test Recipe <span class="subtext">by</span> guest', response.data) # Not logged in, so author is set to 'guest'
         # Removing test data after assertion
-        data = mongo.db.recipes.find_one({'recipe_name': 'test recipe'})
-        if data:
-            mongo.db.recipes.delete_one(data)
+        recipe = mongo.db.recipes.find_one({'recipe_name': 'test recipe'})
+        if recipe:
+            mongo.db.recipes.delete_one(recipe)
 
     def test_add_recipe_as_named_user(self):
         with self.app.session_transaction() as sess:
@@ -211,9 +254,151 @@ class TestRecipesCRUD(unittest.TestCase):
         response = self.app.post('/insert_recipe',data=testdata ,follow_redirects=True)
         self.assertTrue(response.status_code, 302) # Redirected to view_recipe page
         self.assertIn(b'Test Recipe <span class="subtext">by</span> admin', response.data) # Author is set to user logged in (i.e. admin)
-        data = mongo.db.recipes.find_one({'recipe_name': 'test recipe'})
-        if data:
-            mongo.db.recipes.delete_one(data)
+        # Removing test data after assertion
+        recipe = mongo.db.recipes.find_one({'recipe_name': 'test recipe'})
+        if recipe:
+            mongo.db.recipes.delete_one(recipe)
+
+    # To test if the edit recipe page can be reached
+    def test_edit_recipe(self):
+        with self.app.session_transaction() as sess:
+            sess['username'] = 'admin' # Assumed user 'admin' logged in
+        testdata = {
+            "name": "test recipe",
+            "origin": "Japan",
+            "cuisine": "Japanese",
+            "ingredient_1": "ingredient 1",
+            "unit_1": "unit 1",
+            "ingredient_2": "ingredient 2",
+            "unit_2": "unit 2",
+            "instruction_1": "instruction 1"
+        }
+        self.app.post('/insert_recipe',data=testdata ,follow_redirects=True)
+        recipe = mongo.db.recipes.find_one({'recipe_name': 'test recipe'})
+        if recipe:
+            response = self.app.get('/edit_recipe/' + str(recipe['_id']))
+            self.assertEqual(response.status_code, 200) # Edit recipe page can be reached without problem
+            self.assertIn(b'<h3>Edit Recipe</h3>', response.data) # Correct template used
+            mongo.db.recipes.delete_one(recipe)
+        else:
+            self.fail("Were not able to retrieve the recipe created.")
+
+    # To test if update operation actually works
+    def test_update_recipe(self):
+        with self.app.session_transaction() as sess:
+            sess['username'] = 'admin' # Assumed user 'admin' logged in
+        testdata = {
+            "name": "test recipe",
+            "origin": "Japan",
+            "cuisine": "Japanese",
+            "ingredient_1": "ingredient 1",
+            "unit_1": "unit 1",
+            "ingredient_2": "ingredient 2",
+            "unit_2": "unit 2",
+            "instruction_1": "instruction 1"
+        }
+        self.app.post('/insert_recipe',data=testdata ,follow_redirects=True)
+        newdata = {
+            "name": "test recipe new", # Changed name
+            "origin": "Japan",
+            "cuisine": "Japanese",
+            "ingredient_1": "ingredient 1",
+            "unit_1": "unit 1", # Removed ingredient 2
+            "instruction_1": "instruction 1",
+            "instruction_2": "instruction 2" # Added new instruction
+        }
+        recipe = mongo.db.recipes.find_one({'recipe_name': 'test recipe'})
+        if recipe:
+            response = self.app.post('/update_recipe/' + str(recipe['_id']), data=newdata, follow_redirects=True)
+            self.assertIn(b'Test Recipe New', response.data)
+            self.assertIn(b'instruction 2', response.data)
+            self.assertNotIn(b'ingredient 2', response.data)
+            mongo.db.recipes.delete_one({'recipe_name': 'test recipe new'})
+        else:
+            self.fail("Were not able to retrieve the recipe created.")
+
+    # To test if app behaves as expected when user attempt to update recipe other than their own
+    def test_invalid_update_recipe(self):
+        with self.app.session_transaction() as sess:
+            sess['username'] = 'admin' # Assumed user 'admin' logged in
+        testdata = {
+            "name": "test recipe",
+            "origin": "Japan",
+            "cuisine": "Japanese",
+            "ingredient_1": "ingredient 1",
+            "unit_1": "unit 1",
+            "ingredient_2": "ingredient 2",
+            "unit_2": "unit 2",
+            "instruction_1": "instruction 1"
+        }
+        self.app.post('/insert_recipe',data=testdata ,follow_redirects=True)
+        with self.app.session_transaction() as sess:
+            sess['username'] = 'user1' # Assumed user 'user1' logged in
+        newdata = {
+            "name": "test recipe new", # Changed name
+            "origin": "Japan",
+            "cuisine": "Japanese",
+            "ingredient_1": "ingredient 1",
+            "unit_1": "unit 1", # Removed ingredient 2
+            "instruction_1": "instruction 1",
+            "instruction_2": "instruction 2" # Added new instruction
+        }
+        recipe = mongo.db.recipes.find_one({'recipe_name': 'test recipe'})
+        if recipe:
+            response = self.app.get('/edit_recipe/' + str(recipe['_id']), follow_redirects=True)
+            self.assertIn(b"You are not the author of that recipe, hence you are not allowed to edit.", response.data)
+            response = self.app.post('/update_recipe/' + str(recipe['_id']), data=newdata, follow_redirects=True)
+            self.assertIn(b"You are not the author of that recipe, hence you are not allowed to edit.", response.data)
+            mongo.db.recipes.delete_one(recipe)
+        else:
+            self.fail("Were not able to retrieve the recipe created.")
+
+    def test_delete_recipe(self):
+        with self.app.session_transaction() as sess:
+            sess['username'] = 'admin' # Assumed user 'admin' logged in
+        testdata = {
+            "name": "test recipe",
+            "origin": "Japan",
+            "cuisine": "Japanese",
+            "ingredient_1": "ingredient 1",
+            "unit_1": "unit 1",
+            "ingredient_2": "ingredient 2",
+            "unit_2": "unit 2",
+            "instruction_1": "instruction 1"
+        }
+        self.app.post('/insert_recipe',data=testdata ,follow_redirects=True)
+        recipe = mongo.db.recipes.find_one({'recipe_name': 'test recipe'})
+        if recipe:
+            xcount = mongo.db.recipes.count_documents({})
+            response = self.app.get('/delete_recipe/' + str(recipe['_id']), follow_redirects=True)
+            ycount = mongo.db.recipes.count_documents({})
+            self.assertNotEqual(xcount, ycount)
+        else:
+            self.fail("Were not able to retrieve the recipe created.")
+
+    def test_invalid_delete_recipe(self):
+        with self.app.session_transaction() as sess:
+            sess['username'] = 'admin' # Assumed user 'admin' logged in
+        testdata = {
+            "name": "test recipe",
+            "origin": "Japan",
+            "cuisine": "Japanese",
+            "ingredient_1": "ingredient 1",
+            "unit_1": "unit 1",
+            "ingredient_2": "ingredient 2",
+            "unit_2": "unit 2",
+            "instruction_1": "instruction 1"
+        }
+        self.app.post('/insert_recipe',data=testdata ,follow_redirects=True)
+        with self.app.session_transaction() as sess:
+            sess['username'] = 'user1' # Assumed user 'user1' logged in
+        recipe = mongo.db.recipes.find_one({'recipe_name': 'test recipe'})
+        if recipe:
+            response = self.app.get('/delete_recipe/' + str(recipe['_id']), follow_redirects=True)
+            self.assertIn(b'You are not the author of that recipe, hence you are not allowed to delete.', response.data)
+            mongo.db.recipes.delete_one(recipe)
+        else:
+            self.fail("Were not able to retrieve the recipe created.")
 
 if __name__ == '__main__':
     unittest.main()
